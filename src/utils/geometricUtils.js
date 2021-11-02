f84c58a3a8810305a667ae716bfd3a9beea09e5b
Amit <amit@rilke.us>
2021-11-01 17:30:30 -0700

    fix: Adjust boundingBox calculation
    So I started testing it out with larger numbers and turf's methods started creating weird bounding boxes.
    
    I know it's a little past the due date, but I felt dumb not fixing this haha

--- src/utils/geometricUtils.js
+++ src/utils/geometricUtils.js
@@ -1,18 +1,18 @@
 import { CITY_CENTROID, CITY_FEATURE_COLLECTION } from "./constants/cities";
-import bbox from "@turf/bbox";
 import bboxPolygon from "@turf/bbox-polygon";
 import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
 import { convertFeatureToCityFormat } from "./formatUtils";
-import buffer from "@turf/buffer";
-import square from "@turf/square";
 
 const getBoundingBox = (sideLength) => {
   const radius = sideLength / 2;
-  const bufferedCentroid = buffer(CITY_CENTROID, radius, {
-    units: "degrees",
-  });
-  const squareBoundingBox = square(bbox(bufferedCentroid));
-  return bboxPolygon(squareBoundingBox);
+  const bbox = [
+    CITY_CENTROID.geometry.coordinates[0] - radius,
+    CITY_CENTROID.geometry.coordinates[1] - radius,
+    CITY_CENTROID.geometry.coordinates[0] + radius,
+    CITY_CENTROID.geometry.coordinates[1] + radius,
+  ];
+
+  return bboxPolygon(bbox);
 };
 
 const getVisibleCities = (boundingBox, cityName) => {
